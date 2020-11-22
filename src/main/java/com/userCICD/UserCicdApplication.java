2b7128efe6e982f75d490bc292322574171d5916
maityabhishek <a.maity@outlook.com>
2020-11-22 18:30:28 +0530

    first commit

---
+++ src/main/java/com/userCICD/UserCicdApplication.java
@@ -0,0 +1,13 @@
+package com.userCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userCICD/UserCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
